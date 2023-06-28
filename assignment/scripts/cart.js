34e818f0659fb4fb2a2fc7044f2326a6aeca1a4e
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-06-27 23:05:00 -0500

    create basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-
+let basket = [];
+console.log('Items in basket:', basket);
 
 
 
